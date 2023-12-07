5734ec96e0f0cd2d87da91e5c9c974556a51b34a
Nguyen Duc Viet Linh <44722910+vietlinhg7@users.noreply.github.com>
2023-12-07 12:59:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+// Thiet lap thu muc Static
+app.use(express.static(__dirname + '/html'));
+
+// Cau hinh Template Engine
+app.engine(
+    'hbs',
+    expressHbs.engine({
+        layoutsDir: __dirname + '/views/layouts',
+        defaultLayout: 'layout',
+        extname: 'hbs',
+        runtimeOptions: {
+            allowProtoPropertiesByDefault: true,
+        },
+    })
+);
+app.set('view engine', 'hbs');
+
+// Cau hinh cho phep doc du lieu gui len bang phuong thuc POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Thiết lập sử dụng cookies
+app.use(cookieParser('COOKIE_SECRET'));
+
+// Thiết lập sử dụng session và lưu trữ session trên Redis
+app.use(
+    session({
+        secret: 'SESSION_SECRET',
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            secure: false, // if true only transmit cookie over https
+            httpOnly: true, // prevent client side JS from reading the cookie
+            maxAge: 20 * 60 * 1000, // 20m
+        },
+    })
+);
+
+// Chuyen huong route xu ly
+app.use('/', require('./routes/authRouter'));
+
+// Start web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
