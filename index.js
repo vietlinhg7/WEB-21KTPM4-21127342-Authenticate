1520d8f45e6e1f999f5e579e8b81ad005ca2de9c
Nguyen Duc Viet Linh <44722910+vietlinhg7@users.noreply.github.com>
2023-12-07 13:30:51 +0700

    Update port in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 const expressHbs = require('express-handlebars');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
